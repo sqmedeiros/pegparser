0254c85c7e6c15464b644b9fc3e6e2f1d3915e5c
Sergio Queiroz <sqmedeiros@gmail.com>
2019-01-28 11:34:19 -0300

    Testing the files following a alphabetical order

--- util.lua
+++ util.lua
@@ -3,9 +3,27 @@ local lfs = require'lfs'
 local re = require 'relabel'
 local ast = require 'ast'
 
+local verbose = false
+
+local function setVerbose (v)
+	verbose = v
+end
+
 
 local function isSource (file, ext)
 	return string.sub(file, 1, 1) ~= '.' and string.sub(file, #file - #ext + 1) == ext
+end
+
+
+local function getFiles (dir, ext)
+	local t = {}
+	for file in lfs.dir(dir) do
+		if isSource(file, ext) then
+			table.insert(t, file)
+		end
+	end
+	table.sort(t)
+	return t
 end
 
 
@@ -29,40 +47,36 @@ end
 
 
 local function testYes (dir, ext, p)
-	for file in lfs.dir(dir) do
-		if isSource(file, ext) then 
-			print("Yes: ", file)
-			local r, lab, pos = testFile(dir .. file, p)
-			local line, col = '', ''
-			if not r then
-				line, col = re.calcline(getText(dir .. file), pos)
-			end
-			assert(r ~= nil, file .. ': Label: ' .. tostring(lab) .. '  Line: ' .. line .. ' Col: ' .. col)
+	for i, file in ipairs(getFiles(dir, ext)) do
+		print("Yes: ", file)
+		local r, lab, pos = testFile(dir .. file, p)
+		local line, col = '', ''
+		if not r then
+			line, col = re.calcline(getText(dir .. file), pos)
 		end
+		assert(r ~= nil, file .. ': Label: ' .. tostring(lab) .. '  Line: ' .. line .. ' Col: ' .. col)
 	end
 end
 
 
 local function testNo (dir, ext, p, strict, special)
-	for file in lfs.dir(dir) do
-		if isSource(file, ext) then
-			print("No: ", file)
-			local r, lab, pos = testFile(dir .. file, p)
-			if lab ~= nil then
-				io.write('r = ' .. tostring(r) .. ' lab = ' .. tostring(lab))
-			end
-			local line, col = '', ''
-			if not r then
-				line, col = re.calcline(getText(dir .. file), pos)
-				io.write(' line: ' .. line .. ' col: ' .. col)
-			end
-			if strict then
-				assert(r == nil and (matchlabel(file, lab) or matchlabel(file, special)), file .. ': Label: ' .. tostring(lab) .. '  Line: ' .. line .. ' Col: ' .. col)
-			else
-				assert(r == nil, file .. ': Label: ' .. tostring(lab) .. '  Line: ' .. line .. ' Col: ' .. col)
-			end
-			io.write('\n')
+	for i, file in ipairs(getFiles(dir, ext)) do
+		print("No: ", file)
+		local r, lab, pos = testFile(dir .. file, p)
+		if lab ~= nil then
+			io.write('r = ' .. tostring(r) .. ' lab = ' .. tostring(lab))
 		end
+		local line, col = '', ''
+		if not r then
+			line, col = re.calcline(getText(dir .. file), pos)
+			io.write(' line: ' .. line .. ' col: ' .. col)
+		end
+		if strict then
+			assert(r == nil and (matchlabel(file, lab) or matchlabel(file, special)), file .. ': Label: ' .. tostring(lab) .. '  Line: ' .. line .. ' Col: ' .. col)
+		else
+			assert(r == nil, file .. ': Label: ' .. tostring(lab) .. '  Line: ' .. line .. ' Col: ' .. col)
+		end
+		io.write('\n')
 	end
 end
 
@@ -71,23 +85,25 @@ local function testNoRec (dir, ext, p)
 	local irec, ifail = 0, 0
 	local tfail = {}
 
-	for file in lfs.dir(dir) do
-		if isSource(file, ext) then
-			print("No: ", file)
-			local r, lab, pos = testFile(dir .. file, p)
-			if not r then
-				local line, col = re.calcline(getText(dir .. file), pos)
-				io.write('r = ' .. tostring(r) .. ' lab = ' .. tostring(lab))
-				io.write(' line: ' .. line .. ' col: ' .. col)
+	for i, file in ipairs(getFiles(dir, ext)) do
+		print("No: ", file)
+		local r, lab, pos = testFile(dir .. file, p)
+		if not r then
+			local line, col = re.calcline(getText(dir .. file), pos)
+			io.write('r = ' .. tostring(r) .. ' lab = ' .. tostring(lab))
+			io.write(' line: ' .. line .. ' col: ' .. col)
+			io.write('\n')
+     	ifail = ifail + 1
+			tfail[ifail] = { file = file, lab = lab, line = line, col = col }
+		else
+			if verbose then
+				io.write(getText(dir .. file))
+			end
+			irec = irec + 1
+			if type(r) == 'table' then
+				ast.printAST(r)
 				io.write('\n')
-      	ifail = ifail + 1
-				tfail[ifail] = { file = file, lab = lab, line = line, col = col }
-			else
-				irec = irec + 1
-				if type(r) == 'table' then
-					ast.printAST(r)
-					io.write('\n')
-				end
+				io.write('\n')
 			end
 		end
 	end
@@ -104,4 +120,5 @@ return {
 	testNo = testNo,
 	testNoRec = testNoRec,
 	testFile = testFile,
+	setVerbose = setVerbose,
 }
