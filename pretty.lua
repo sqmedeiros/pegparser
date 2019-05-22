d7d675d459039413ce414e69aee946de0a0c72b3
Sergio <sqmedeiros@gmail.com>
2019-05-22 11:12:05 -0300

    Printing an expression property

--- pretty.lua
+++ pretty.lua
@@ -2,11 +2,21 @@ local pretty = {}
 
 local parser = require 'parser'
 
+local property
+
+local function printProp (p)
+	if property and p[property] then
+		--if p.ban then assert(false, p.tag .. ', ' .. tostring(property) .. ', ' .. tostring(p[property])) end
+		return '_' .. tostring(property)
+	end
+	return ''
+end
+
 local function printp (p, flag)
 	if p.tag == 'empty' then
 		return "''"
 	elseif p.tag == 'char' then
-		return p.p2 .. p.p1 .. p.p2
+		return p.p2 .. p.p1 .. p.p2 .. printProp(p) 
 	elseif p.tag == 'any' then
 		return "."
 	elseif p.tag == 'set' then
@@ -24,21 +34,26 @@ local function printp (p, flag)
 	elseif p.tag == 'anonCap' then
 		return '{:' .. printp(p.p1, flag) .. ':}'
 	elseif p.tag == 'var' then
-		return p.p1
+		return p.p1 .. printProp(p)
 	elseif p.tag == 'ord' then
-		local s1 = printp(p.p1, flag)
-		local s2 = printp(p.p2, flag)
+		local s1 = printp(p.p1, flag) 
+		local s2 = printp(p.p2, flag) 
 		if p.p2.tag == 'throw' then
 			if not flag then
-				return '[' .. s1 .. ']^' .. string.sub(s2, 2)
+				return '[' .. s1 .. ']^' .. string.sub(s2, 2) .. printProp(p) 
 			else
 					if p.p1.tag == 'ord' then
 						s1 = '(' .. s1 .. ')'
 					end
-					return s1 .. '^' .. string.sub(s2, 2)
+					return s1 .. '^' .. string.sub(s2, 2) .. printProp(p)
 			end
 		else
-			return  s1 .. '  /  ' .. s2
+			local s = printProp(p)
+			if s == '' then
+				return  s1 .. '  /  ' .. s2
+			else
+				return  '(' .. s1 .. '  /  ' .. s2 .. ')' .. printProp(p)
+			end
 		end
 	elseif p.tag == 'con' then
 		local s1 = printp(p.p1, flag)
@@ -52,7 +67,7 @@ local function printp (p, flag)
 		else
 			s = s .. ' ' .. s2
 		end
-		return s
+		return s --.. printProp(p)
 	elseif p.tag == 'and' or p.tag == 'not' then
 		local s = printp(p.p1, flag)
 		if parser.isSimpleExp(p.p1) then
@@ -65,9 +80,9 @@ local function printp (p, flag)
 	elseif p.tag == "star" or p.tag == 'plus' or p.tag == 'opt' then
 		local s = printp(p.p1, flag)
 		if parser.isSimpleExp(p.p1) then
-			return s .. parser.repSymbol(p)
+			return s .. parser.repSymbol(p) .. printProp(p)
 		else
-			return '(' .. s .. ')' .. parser.repSymbol(p)
+			return '(' .. s .. ')' .. parser.repSymbol(p) .. printProp(p)
 		end
   elseif p.tag == 'throw' then
     return '^' .. p.p1
@@ -79,7 +94,9 @@ local function printp (p, flag)
 	end
 end
 
-local function printg (g, flagthrow)
+local function printg (g, flagthrow, k)
+	property = k
+	print("Property ", k)
 	local t = {}
 	for i, v in ipairs(g.plist) do
 		table.insert(t, string.format("%-15s <-  %s", v, printp(g.prules[v], flagthrow)))
