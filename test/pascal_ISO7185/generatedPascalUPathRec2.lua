e679f8ce1bd63cf3e5bdf2046eaa55b60e70aad8
gilneyjr <gilneyjnr@gmail.com>
2019-08-31 14:34:17 -0300

    Analyzing Java and Pascal UPathPrefLast

---
+++ test/pascal_ISO7185/generatedPascalUPathRec2.lua
@@ -0,0 +1,229 @@
+local m = require 'init'
+local errinfo = require 'syntax_errors'
+local pretty = require 'pretty'
+local coder = require 'coder'
+local first = require 'first'
+local recovery = require 'recovery'
+local lfs = require'lfs'
+local re = require'relabel'
+local ast = require'ast'
+local util = require'util'
+
+--[[ 
+	Inserted: 64 labels (64 correct)
+]]
+
+g = [[
+program         <-  SKIP head^Err_001 decs block^Err_002 Dot^Err_003 (!.)^Err_EOF
+head            <-  PROGRAM^Err_004 Id^Err_005 (LPar ids^Err_006 RPar^Err_007)? Semi^Err_008
+decs            <-  labelDecs constDefs typeDefs varDecs procAndFuncDecs
+ids             <-  Id (Comma Id)*
+labelDecs       <-  (LABEL labels^Err_009 Semi^Err_010)?
+labels          <-  label^Err_011 (Comma label^Err_012)*
+label           <-  UInt
+constDefs       <-  (CONST constDef^Err_013 Semi^Err_014 (constDef Semi^Err_015)*)?
+constDef        <-  Id Eq^Err_016 const^Err_017
+const           <-  Sign? (UNumber  /  Id)  /  String
+typeDefs        <-  (TYPE typeDef^Err_018 Semi^Err_019 (typeDef Semi^Err_020)*)?
+typeDef         <-  Id Eq^Err_021 type^Err_022
+type            <-  newType  /  Id
+newType         <-  newOrdinalType  /  newStructuredType  /  newPointerType
+newOrdinalType  <-  enumType  /  subrangeType
+newStructuredType <-  PACKED? unpackedStructuredType
+newPointerType  <-  Pointer Id
+enumType        <-  LPar ids RPar
+subrangeType    <-  const DotDot const
+unpackedStructuredType <-  arrayType  /  recordType  /  setType  /  fileType
+arrayType       <-  ARRAY LBrack^Err_023 ordinalType^Err_024 (Comma ordinalType^Err_025)* RBrack^Err_026 OF^Err_027 type^Err_028
+recordType      <-  RECORD fieldList END^Err_029
+setType         <-  SET OF^Err_030 ordinalType^Err_031
+fileType        <-  FILE OF^Err_032 type^Err_033
+ordinalType     <-  (newOrdinalType  /  Id)^Err_034
+fieldList       <-  ((fixedPart (Semi variantPart)?  /  variantPart) Semi?)?
+fixedPart       <-  varDec (Semi varDec)*
+variantPart     <-  CASE Id (Colon Id)? OF variant (Semi variant)*
+variant         <-  consts Colon LPar fieldList RPar
+consts          <-  const (Comma const)*
+varDecs         <-  (VAR varDec Semi (varDec Semi)*)?
+varDec          <-  ids Colon type
+procAndFuncDecs <-  ((procDec  /  funcDec) Semi)*
+procDec         <-  procHeading Semi (decs block  /  Id)
+procHeading     <-  PROCEDURE Id^Err_035 formalParams?
+funcDec         <-  funcHeading Semi (decs block  /  Id)
+funcHeading     <-  FUNCTION Id^Err_036 formalParams? Colon^Err_037 type^Err_038
+formalParams    <-  LPar formalParamsSection^Err_039 (Semi formalParamsSection^Err_040)* RPar^Err_041
+formalParamsSection <-  (VAR? ids Colon^Err_042 Id^Err_043  /  procHeading  /  funcHeading)^Err_044
+block           <-  BEGIN stmts END^Err_045
+stmts           <-  stmt (Semi stmt)*
+stmt            <-  (label Colon)? (simpleStmt  /  structuredStmt)?
+simpleStmt      <-  assignStmt  /  procStmt  /  gotoStmt
+assignStmt      <-  var Assign expr
+var             <-  Id (LBrack expr (Comma expr)* RBrack  /  Dot Id  /  Pointer)*
+procStmt        <-  Id params?
+params          <-  LPar (param (Comma param)*)? RPar
+param           <-  expr (Colon expr)? (Colon expr)?
+gotoStmt        <-  GOTO label^Err_046
+structuredStmt  <-  block  /  conditionalStmt  /  repetitiveStmt  /  withStmt
+conditionalStmt <-  ifStmt  /  caseStmt
+ifStmt          <-  IF expr^Err_047 THEN^Err_048 stmt (ELSE stmt)?
+caseStmt        <-  CASE expr OF caseListElement (Semi caseListElement)* Semi? END
+caseListElement <-  consts Colon stmt
+repetitiveStmt  <-  repeatStmt  /  whileStmt  /  forStmt
+repeatStmt      <-  REPEAT stmts UNTIL^Err_049 expr^Err_050
+whileStmt       <-  WHILE expr^Err_051 DO^Err_052 stmt
+forStmt         <-  FOR Id^Err_053 Assign^Err_054 expr^Err_055 (TO  /  DOWNTO)^Err_056 expr^Err_057 DO^Err_058 stmt
+withStmt        <-  WITH var^Err_059 (Comma var^Err_060)* DO^Err_061 stmt
+expr            <-  simpleExpr (RelOp simpleExpr^Err_062)?
+simpleExpr      <-  Sign? term (AddOp term^Err_063)*
+term            <-  factor (MultOp factor^Err_064)*
+factor          <-  NOT* (funcCall  /  var  /  unsignedConst  /  setConstructor  /  LPar expr RPar)
+unsignedConst   <-  UNumber  /  String  /  Id  /  NIL
+funcCall        <-  Id params
+setConstructor  <-  LBrack (memberDesignator (Comma memberDesignator)*)? RBrack
+memberDesignator <-  expr (DotDot expr)?
+AddOp           <-  '+'  /  '-'  /  OR
+Assign          <-  ':='
+Dot             <-  '.'
+DotDot          <-  '..'
+CloseComment    <-  '*)'  /  '}'
+Colon           <-  ':'
+Comma           <-  ','
+COMMENT         <-  OpenComment (!CloseComment .)* CloseComment
+Eq              <-  '='
+BodyId          <-  [a-zA-Z0-9]
+Id              <-  !Reserved [a-zA-Z] [a-zA-Z0-9]*
+LBrack          <-  '['
+LPar            <-  '('
+MultOp          <-  '*'  /  '/'  /  DIV  /  MOD  /  AND
+OpenComment     <-  '(*'  /  '{'
+Pointer         <-  '^'
+RBrack          <-  ']'
+RelOp           <-  '<='  /  '='  /  '<>'  /  '>='  /  '>'  /  '<'  /  IN
+RPar            <-  ')'
+Semi            <-  ';'
+Sign            <-  '+'  /  '-'
+String          <-  "'" (!"'" .)* "'"
+UInt            <-  [0-9]+
+UNumber         <-  UReal  /  UInt
+UReal           <-  [0-9]+ ('.' [0-9]+ ([Ee] ('+'  /  '-') [0-9]+)?  /  [Ee] ('+'  /  '-') [0-9]+)
+Reserved        <-  AND  /  ARRAY  /  BEGIN  /  CONST  /  CASE  /  DIV  /  DO  /  DOWNTO  /  ELSE  /  END  /  FILE  /  FOR  /  FUNCTION  /  GOTO  /  IF  /  IN  /  LABEL  /  MOD  /  NIL  /  NOT  /  OF  /  OR  /  PACKED  /  PROCEDURE  /  PROGRAM  /  RECORD  /  REPEAT  /  SET  /  THEN  /  TO  /  TYPE  /  UNTIL  /  VAR  /  WHILE  /  WITH
+AND             <-  [Aa] [Nn] [Dd] !BodyId
+ARRAY           <-  [Aa] [Rr] [Rr] [Aa] [Yy] !BodyId
+BEGIN           <-  [Bb] [Ee] [Gg] [Ii] [Nn] !BodyId
+CASE            <-  [Cc] [Aa] [Ss] [Ee] !BodyId
+CONST           <-  [Cc] [Oo] [Nn] [Ss] [Tt] !BodyId
+DIV             <-  [Dd] [Ii] [Vv] !BodyId
+DO              <-  [Dd] [Oo] !BodyId
+DOWNTO          <-  [Dd] [Oo] [Ww] [Nn] [Tt] [Oo] !BodyId
+ELSE            <-  [Ee] [Ll] [Ss] [Ee] !BodyId
+END             <-  [Ee] [Nn] [Dd] !BodyId
+FILE            <-  [Ff] [Ii] [Ll] [Ee] !BodyId
+FOR             <-  [Ff] [Oo] [Rr] !BodyId
+FUNCTION        <-  [Ff] [Uu] [Nn] [Cc] [Tt] [Ii] [Oo] [Nn] !BodyId
+GOTO            <-  [Gg] [Oo] [Tt] [Oo] !BodyId
+IF              <-  [Ii] [Ff] !BodyId
+IN              <-  [Ii] [Nn] !BodyId
+LABEL           <-  [Ll] [Aa] [Bb] [Ee] [Ll] !BodyId
+MOD             <-  [Mm] [Oo] [Dd] !BodyId
+NIL             <-  [Nn] [Ii] [Ll] !BodyId
+NOT             <-  [Nn] [Oo] [Tt] !BodyId
+OF              <-  [Oo] [Ff] !BodyId
+OR              <-  [Oo] [Rr] !BodyId
+PACKED          <-  [Pp] [Aa] [Cc] [Kk] [Ee] [Dd] !BodyId
+PROCEDURE       <-  [Pp] [Rr] [Oo] [Cc] [Ee] [Dd] [Uu] [Rr] [Ee] !BodyId
+PROGRAM         <-  [Pp] [Rr] [Oo] [Gg] [Rr] [Aa] [Mm] !BodyId
+RECORD          <-  [Rr] [Ee] [Cc] [Oo] [Rr] [Dd] !BodyId
+REPEAT          <-  [Rr] [Ee] [Pp] [Ee] [Aa] [Tt] !BodyId
+SET             <-  [Ss] [Ee] [Tt] !BodyId
+THEN            <-  [Tt] [Hh] [Ee] [Nn] !BodyId
+TO              <-  [Tt] [Oo] !BodyId
+TYPE            <-  [Tt] [Yy] [Pp] [Ee] !BodyId
+UNTIL           <-  [Uu] [Nn] [Tt] [Ii] [Ll] !BodyId
+VAR             <-  [Vv] [Aa] [Rr] !BodyId
+WHILE           <-  [Ww] [Hh] [Ii] [Ll] [Ee] !BodyId
+WITH            <-  [Ww] [Ii] [Tt] [Hh] !BodyId
+SPACE           <-  [ 	
+
+]  /  COMMENT
+SKIP            <-  ([ 	
+
+]  /  COMMENT)*
+Token           <-  WITH  /  WHILE  /  VAR  /  UReal  /  UNumber  /  UNTIL  /  UInt  /  TYPE  /  TO  /  THEN  /  String  /  Sign  /  Semi  /  SET  /  Reserved  /  RelOp  /  RPar  /  REPEAT  /  RECORD  /  RBrack  /  Pointer  /  PROGRAM  /  PROCEDURE  /  PACKED  /  OpenComment  /  OR  /  OF  /  NOT  /  NIL  /  MultOp  /  MOD  /  LPar  /  LBrack  /  LABEL  /  Id  /  IN  /  IF  /  GOTO  /  FUNCTION  /  FOR  /  FILE  /  Eq  /  END  /  ELSE  /  DotDot  /  Dot  /  DOWNTO  /  DO  /  DIV  /  Comma  /  Colon  /  CloseComment  /  CONST  /  COMMENT  /  CASE  /  BodyId  /  BEGIN  /  Assign  /  AddOp  /  ARRAY  /  AND
+EatToken        <-  (Token  /  (!SPACE .)+) SKIP
+Err_EOF         <-  (!!. EatToken)*
+Err_001         <-  (!(VAR  /  TYPE  /  PROCEDURE  /  LABEL  /  FUNCTION  /  CONST  /  BEGIN) EatToken)*
+Err_002         <-  (!Dot EatToken)*
+Err_003         <-  (!(!.) EatToken)*
+Err_004         <-  (!Id EatToken)*
+Err_005         <-  (!(Semi  /  LPar) EatToken)*
+Err_006         <-  (!RPar EatToken)*
+Err_007         <-  (!Semi EatToken)*
+Err_008         <-  (!(VAR  /  TYPE  /  PROCEDURE  /  LABEL  /  FUNCTION  /  CONST  /  BEGIN) EatToken)*
+Err_009         <-  (!Semi EatToken)*
+Err_010         <-  (!(VAR  /  TYPE  /  PROCEDURE  /  FUNCTION  /  CONST  /  BEGIN) EatToken)*
+Err_011         <-  (!(Semi  /  Comma) EatToken)*
+Err_012         <-  (!Semi EatToken)*
+Err_013         <-  (!Semi EatToken)*
+Err_014         <-  (!(VAR  /  TYPE  /  PROCEDURE  /  Id  /  FUNCTION  /  BEGIN) EatToken)*
+Err_015         <-  (!(VAR  /  TYPE  /  PROCEDURE  /  FUNCTION  /  BEGIN) EatToken)*
+Err_016         <-  (!(UNumber  /  String  /  Sign  /  Id) EatToken)*
+Err_017         <-  (!Semi EatToken)*
+Err_018         <-  (!Semi EatToken)*
+Err_019         <-  (!(VAR  /  PROCEDURE  /  Id  /  FUNCTION  /  BEGIN) EatToken)*
+Err_020         <-  (!(VAR  /  PROCEDURE  /  FUNCTION  /  BEGIN) EatToken)*
+Err_021         <-  (!(UNumber  /  String  /  Sign  /  SET  /  RECORD  /  Pointer  /  PACKED  /  LPar  /  Id  /  FILE  /  ARRAY) EatToken)*
+Err_022         <-  (!Semi EatToken)*
+Err_023         <-  (!(UNumber  /  String  /  Sign  /  LPar  /  Id) EatToken)*
+Err_024         <-  (!(RBrack  /  Comma) EatToken)*
+Err_025         <-  (!RBrack EatToken)*
+Err_026         <-  (!OF EatToken)*
+Err_027         <-  (!(UNumber  /  String  /  Sign  /  SET  /  RECORD  /  Pointer  /  PACKED  /  LPar  /  Id  /  FILE  /  ARRAY) EatToken)*
+Err_028         <-  (!(Semi  /  RPar  /  END) EatToken)*
+Err_029         <-  (!(Semi  /  RPar  /  END) EatToken)*
+Err_030         <-  (!(UNumber  /  String  /  Sign  /  LPar  /  Id) EatToken)*
+Err_031         <-  (!(Semi  /  RPar  /  END) EatToken)*
+Err_032         <-  (!(UNumber  /  String  /  Sign  /  SET  /  RECORD  /  Pointer  /  PACKED  /  LPar  /  Id  /  FILE  /  ARRAY) EatToken)*
+Err_033         <-  (!(Semi  /  RPar  /  END) EatToken)*
+Err_034         <-  (!(Semi  /  RPar  /  RBrack  /  END  /  Comma) EatToken)*
+Err_035         <-  (!(Semi  /  RPar  /  LPar) EatToken)*
+Err_036         <-  (!(LPar  /  Colon) EatToken)*
+Err_037         <-  (!(UNumber  /  String  /  Sign  /  SET  /  RECORD  /  Pointer  /  PACKED  /  LPar  /  Id  /  FILE  /  ARRAY) EatToken)*
+Err_038         <-  (!(Semi  /  RPar) EatToken)*
+Err_039         <-  (!(Semi  /  RPar) EatToken)*
+Err_040         <-  (!RPar EatToken)*
+Err_041         <-  (!(Semi  /  RPar  /  Colon) EatToken)*
+Err_042         <-  (!Id EatToken)*
+Err_043         <-  (!(Semi  /  RPar) EatToken)*
+Err_044         <-  (!(Semi  /  RPar) EatToken)*
+Err_045         <-  (!(UNTIL  /  Semi  /  END  /  ELSE  /  Dot) EatToken)*
+Err_046         <-  (!(UNTIL  /  Semi  /  END  /  ELSE) EatToken)*
+Err_047         <-  (!THEN EatToken)*
+Err_048         <-  (!(WITH  /  WHILE  /  UNTIL  /  UInt  /  Semi  /  REPEAT  /  Id  /  IF  /  GOTO  /  FOR  /  END  /  ELSE  /  CASE  /  BEGIN) EatToken)*
+Err_049         <-  (!(UNumber  /  String  /  Sign  /  NOT  /  NIL  /  LPar  /  LBrack  /  Id) EatToken)*
+Err_050         <-  (!(UNTIL  /  Semi  /  END  /  ELSE) EatToken)*
+Err_051         <-  (!DO EatToken)*
+Err_052         <-  (!(WITH  /  WHILE  /  UNTIL  /  UInt  /  Semi  /  REPEAT  /  Id  /  IF  /  GOTO  /  FOR  /  END  /  ELSE  /  CASE  /  BEGIN) EatToken)*
+Err_053         <-  (!Assign EatToken)*
+Err_054         <-  (!(UNumber  /  String  /  Sign  /  NOT  /  NIL  /  LPar  /  LBrack  /  Id) EatToken)*
+Err_055         <-  (!(TO  /  DOWNTO) EatToken)*
+Err_056         <-  (!(UNumber  /  String  /  Sign  /  NOT  /  NIL  /  LPar  /  LBrack  /  Id) EatToken)*
+Err_057         <-  (!DO EatToken)*
+Err_058         <-  (!(WITH  /  WHILE  /  UNTIL  /  UInt  /  Semi  /  REPEAT  /  Id  /  IF  /  GOTO  /  FOR  /  END  /  ELSE  /  CASE  /  BEGIN) EatToken)*
+Err_059         <-  (!(DO  /  Comma) EatToken)*
+Err_060         <-  (!DO EatToken)*
+Err_061         <-  (!(WITH  /  WHILE  /  UNTIL  /  UInt  /  Semi  /  REPEAT  /  Id  /  IF  /  GOTO  /  FOR  /  END  /  ELSE  /  CASE  /  BEGIN) EatToken)*
+Err_062         <-  (!(UNTIL  /  TO  /  THEN  /  Semi  /  RPar  /  RBrack  /  OF  /  END  /  ELSE  /  DotDot  /  DOWNTO  /  DO  /  Comma  /  Colon) EatToken)*
+Err_063         <-  (!(UNTIL  /  TO  /  THEN  /  Semi  /  RelOp  /  RPar  /  RBrack  /  OF  /  END  /  ELSE  /  DotDot  /  DOWNTO  /  DO  /  Comma  /  Colon) EatToken)*
+Err_064         <-  (!(UNTIL  /  TO  /  THEN  /  Semi  /  RelOp  /  RPar  /  RBrack  /  OF  /  END  /  ELSE  /  DotDot  /  DOWNTO  /  DO  /  Comma  /  Colon  /  AddOp) EatToken)*
+]]
+
+local g = m.match(g)
+local p = coder.makeg(g, 'ast')
+
+local dir = lfs.currentdir() .. '/test/pascal_ISO7185/test/yes/' 
+util.testYes(dir, 'pas', p)
+
+util.setVerbose(true)
+print""
+local dir = lfs.currentdir() .. '/test/pascal_ISO7185/test/no/' 
+util.testNoRec(dir, 'pas', p)
