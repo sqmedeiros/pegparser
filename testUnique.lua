cbed9907cf91f0311a4bd6fab864a8ddfd19aecd
Sergio <sqmedeiros@gmail.com>
2020-01-31 14:27:52 -0300

    Adding tests

--- testUnique.lua
+++ testUnique.lua
@@ -10,7 +10,7 @@ local function autoLab (s)
 	return pretty.printg(gupath, true, nil, "notLex")
 end
 
-local function assertEqual (manual, auto)	
+local function assertEqual (manual, auto)
 	assert(util.removeSpace(manual) == util.removeSpace(auto),
          'Not equal!\nManual:\n' .. manual .. '\n' .. 'Auto:\n' .. auto)
 end
@@ -33,6 +33,50 @@ s <- ('a' 'b' / 'a' 'c')*
 
 manualLab = [[
 s <- ('a' 'b' / 'a' 'c'^Err_001)*
+]]
+
+assertEqual(manualLab, autoLab(s))
+
+
+-- Lexical rules start in uppercase.
+-- The current algorithm does not annotate lexical rules,
+-- thus 'assertEqual' does not check them.
+-- We should not list the lexical rules in 'manualLab'.
+s = [[
+s <- (A 'b' / A 'c')*
+A <- 'a'
+]]
+
+manualLab = [[
+s <- (A 'b' / A 'c'^Err_001)*
+]]
+
+assertEqual(manualLab, autoLab(s))
+
+
+-- Assumes the FIRST set of a lexical rule A is disjoint
+-- from all FIRST sets which do not contain A
+s = [[
+s <- (A 'b' / B 'c')*
+A <- 'a'
+B <- 'a'
+]]
+
+manualLab = [[
+s <- (A 'b'^Err_001 / B 'c'^Err_002)*
+]]
+
+assertEqual(manualLab, autoLab(s))
+
+
+-- FIRST(A) is different from FIRST('a')
+s = [[
+s <- (A 'b' / 'a' 'c')*
+A <- 'a'
+]]
+
+manualLab = [[
+s <- (A 'b'^Err_001 / 'a' 'c'^Err_002)*
 ]]
 
 assertEqual(manualLab, autoLab(s))
@@ -65,14 +109,28 @@ s = [[
 ]]
 
 manualLab = [[
-	s    <- ('if' '(' exp ')' cmd 'else' cmd^Err_001  /  'if' '('^Err_002 exp^Err_003 ')'^Err_004 cmd^Err_005)
-  exp  <- '0' / '1'
-  cmd  <- 'print'
+	s    <- ('if' '('^Err_001 exp^Err_002 ')'^Err_003 cmd^Err_004 'else' cmd^Err_005  /  'if' '('^Err_006 exp^Err_007 ')'^Err_008 cmd^Err_009)^Err_010
+  exp  <- ('0' / '1')^Err_011
+  cmd  <- 'print'^Err_012
 ]]
 
 assertEqual(manualLab, autoLab(s))
 
 
+-- The algorithm must not annotate 'else'
+-- In case the algorithm tries to annotate symbols with the same prefix
+-- and it can annotate, in the first alternative, symbols '(', exp, ')', cmd
+s = [[
+	s    <- 'a'* x  /  'b' x
+  x  <- 'print'
+]]
+
+manualLab = [[
+	s    <- ('a'* x  /  'b' x^Err_001)^Err_002
+  x  <- 'print'
+]]
+
+assertEqual(manualLab, autoLab(s))
 
 
 
