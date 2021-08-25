cd5c6fb19b6c86e8077826310067853383850154
Sérgio Queiroz <sqmedeiros@gmail.com>
2021-08-25 15:32:48 -0300

    Creating more types of nodes

--- node.lua
+++ node.lua
@@ -48,8 +48,10 @@ end
 
 
 function Node.newCon (...)
-	if #{...} > 1 then
-		return Node.new('con', {...})
+	local l = {...}
+	
+	if #l > 1 then
+		return Node.new('con', l)
 	else
 		return ...
 	end
@@ -57,12 +59,69 @@ end
 
 
 function Node.newChoice (...)
-	if #{...} > 1 then
-		return Node.new('choice', {...})
+	local l = {...}
+	
+	if #l > 1 then
+		return Node.new('choice', l)
 	else
 		return ...
 	end
 end
+
+
+function Node.newOpt (p)
+	return Node.new('opt', p)
+end
+
+
+function Node.newStar (p)
+	return Node.new('star', p)
+end
+
+
+function Node.newPlus (p)
+	return Node.new('plus', p)
+end
+
+
+function Node.newThrow (lab)
+	return Node.new('throw', lab)
+end
+
+
+function Node.newDef (v)
+	return Node.new('def', v)
+end
+
+
+function Node.newConstCap (p)
+	return Node.new('constCap', p)
+end
+
+function Node.newPosCap ()
+	return Node.new('posCap')
+end
+
+function Node.newSimpCap (p)
+	return Node.new('simpCap', p)
+end
+
+function Node.newTabCap(p)
+	return Node.new('tabCap', p)
+end
+
+function Node.newAnonCap (p)
+	return Node.new('anonCap', p)
+end
+
+function Node.newNameCap (p1, p2)
+	return Node.new('nameCap', p1, p2)
+end
+
+function Node.newDiscardCap (p)
+	return Node.new('funCap', p)
+end
+
 
 
 return Node
--- spec/node_spec.lua
+++ spec/node_spec.lua
@@ -2,7 +2,7 @@ local node = require"node"
 
 describe("Testing #node", function()
 	
-	test("Creating the non-recursive nodes", function()
+	test("Creating nodes", function()
 		-- Empty
 		local nodeEmpty = { tag = "empty" }
 		assert.same(node.newEmpty(), nodeEmpty)
@@ -14,9 +14,9 @@ describe("Testing #node", function()
 		assert.same(node.new"any", nodeAny)
 		
 		-- Char
-		local nodeChar = { tag = "char", p = "foo" }
-		assert.same(node.newChar"foo", nodeChar)
-		assert.same(node.new("char", "foo"), nodeChar)
+		local nodeChar = { tag = "char", p = "'foo'" }
+		assert.same(node.newChar"'foo'", nodeChar)
+		assert.same(node.new("char", "'foo'"), nodeChar)
 		
 		-- Class/Set of Characteres
 		local nodeSet =  { tag = "set", p = { "a-e", "0-9", 42 } } 
@@ -26,7 +26,89 @@ describe("Testing #node", function()
 		-- Var
 		local nodeVar = { tag = "var", p = "foo" }
 		assert.same(node.newVar("foo"), nodeVar)
-		assert.same(node.new('var', "foo"), nodeVar)
+		assert.same(node.new("var", "foo"), nodeVar)
+		
+		-- And predicate
+		local nodeAnd = { tag = "and", p = nodeVar }
+		assert.same(node.newAnd(node.newVar"foo"), nodeAnd)
+		assert.same(node.new("and", node.new("var", "foo")), nodeAnd)
+		
+		-- Not predicate
+		local nodeNot = { tag = "not", p = nodeChar }
+		assert.same(node.newNot(node.newChar"'foo'"), nodeNot)
+		assert.same(node.new("not", node.new("char", "'foo'")), nodeNot)
+		
+		-- Concatetation
+		local nodeCon = { tag = "con", p = { nodeChar, nodeVar } }
+		local char = node.newChar
+		local var = node.newVar
+		assert.same(node.newCon(char"'foo'", var"foo"), nodeCon)
+		assert.same(node.new("con", { char"'foo'", var"foo"} ), nodeCon)
+		
+		-- Choice
+		local nodeChoice = { tag = "choice", p = { nodeSet, nodeAnd } }
+		assert.same(node.newChoice(nodeSet, nodeAnd), nodeChoice)
+		assert.same(node.new("choice", { nodeSet, nodeAnd } ), nodeChoice)
+		
+		-- "a" A / B "B"
+		local con1 = { tag = "con", p = { { tag = "char", p = "a" }, { tag = "var", p = "A" } } }
+		local con2 = { tag = "con", p = { { tag = "var", p = "B" }, { tag = "char", p = "B" } } }  
+		local choice = { tag = "choice", p = { con1, con2 } }  
+		assert.same(node.newChoice(node.newCon(char"a", var"A"), node.newCon(var"B", char"B")), choice)
+		
+		-- Optional: p?
+		local nodeOpt = { tag = "opt", p = nodeVar }
+		assert.same(node.newOpt(node.newVar"foo"), nodeOpt)
+		assert.same(node.new("opt", node.new("var", "foo")), nodeOpt)
+		
+		-- Zero or more: p*
+		local nodeStar = { tag = "star", p = nodeChar }
+		assert.same(node.newStar(node.newChar"'foo'"), nodeStar)
+		assert.same(node.new("star", node.new("char", "'foo'")), nodeStar)
+		
+		-- One or more: p*
+		local nodePlus = { tag = "plus", p = nodeVar }
+		assert.same(node.newPlus(node.newVar"foo"), nodePlus)
+		assert.same(node.new("plus", node.new("var", "foo")), nodePlus)
+		
+		-- Throw
+		local nodeThrow = { tag = "throw", p = "lua" }
+		assert.same(node.newThrow"lua", nodeThrow)
+		assert.same(node.new("throw", "lua"), nodeThrow)
+		
+		-- Def
+		local nodeDef = { tag = "def", p = "lua" }
+		assert.same(node.newDef"lua", nodeDef)
+		assert.same(node.new("def", "lua"), nodeDef)
+		
+		-- Captures
+		local nodeConstCap = { tag = "constCap", p = nodeChar }
+		assert.same(node.newConstCap(nodeChar), nodeConstCap)
+		assert.same(node.new("constCap", nodeChar), nodeConstCap)
+		
+		local nodePosCap = { tag = "posCap" }
+		assert.same(node.newPosCap(), nodePosCap)
+		assert.same(node.new("posCap"), nodePosCap)
+
+		local nodeSimpCap = { tag = "simpCap", p = nodeChar }
+		assert.same(node.newSimpCap(nodeChar), nodeSimpCap)
+		assert.same(node.new("simpCap", nodeChar), nodeSimpCap)
+
+		local nodeTabCap = { tag = "tabCap", p = nodeChar }
+		assert.same(node.newTabCap(nodeChar), nodeTabCap)
+		assert.same(node.new("tabCap", nodeChar), nodeTabCap)
+
+		local nodeAnonCap = { tag = "anonCap", p = nodeChar }
+		assert.same(node.newAnonCap(nodeChar), nodeAnonCap)
+		assert.same(node.new("anonCap", nodeChar), nodeAnonCap)
+
+		local nodeNameCap = { tag = "nameCap", p = nodeChar }
+		assert.same(node.newNameCap(nodeChar), nodeNameCap)
+		assert.same(node.new("nameCap", nodeChar), nodeNameCap)
+
+		local nodeDiscardCap = { tag = "funCap", p = nodeChar }
+		assert.same(node.newDiscardCap(nodeChar), nodeDiscardCap)
+		assert.same(node.new("funCap", nodeChar), nodeDiscardCap)
 		
 	end)
 	
