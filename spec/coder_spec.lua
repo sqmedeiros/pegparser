8a9fc0a3978a1f0679d34809d449d10ddf6dd96e
Sérgio Queiroz de Medeiros <sergio@admins-iMac.local>
2022-05-09 06:59:15 -0300

    Coder spec

--- spec/coder_spec.lua
+++ spec/coder_spec.lua
@@ -159,6 +159,50 @@ describe("Testing #coder", function()
         assert.equal(lpegParser:match(s), #s + 1)
 	end)
 
+    test("Example  based on DOT Grammar", function()
+        local g = Parser.match[[
+stmt_list   <-   (stmt ';'? )* 
+stmt        <-   edge_stmt   /  attr_stmt   /  id '=' id   /  node_stmt   /  subgraph
+attr_stmt   <-   (GRAPH   /  NODE   /  EDGE ) attr_list 
+attr_list   <-   ('[' a_list? ']' )+ 
+a_list   <-   (id ('=' id )? ','? )+ 
+edge_stmt   <-   (node_id   /  subgraph ) edgeRHS attr_list? 
+edgeRHS   <-   (edgeop (node_id   /  subgraph ) )+ 
+edgeop   <-   '->'   /  '--' 
+node_stmt   <-   node_id attr_list? 
+node_id   <-   id port? 
+port   <-   ':' id (':' id )? 
+subgraph   <-   (SUBGRAPH id? )? '{' stmt_list '}' 
+id   <-   ID   /  STRING   /  HTML_STRING   /  NUMBER 
+STRICT   <-   [Ss] [Tt] [Rr] [Ii] [Cc] [Tt]
+GRAPH   <-   [Gg] [Rr] [Aa] [Pp] [Hh]
+DIGRAPH   <-   [Dd] [Ii] [Gg] [Rr] [Aa] [Pp] [Hh]
+NODE   <-   [Nn] [Oo] [Dd] [Ee]
+EDGE   <-   [Ee] [Dd] [Gg] [Ee]
+SUBGRAPH   <-   [Ss] [Uu] [Bb] [Gg] [Rr] [Aa] [Pp] [Hh]
+NUMBER   <-   '-'? ('.' DIGIT+  /  DIGIT+ ('.' DIGIT*)?)
+DIGIT   <-   [0-9]
+STRING   <-   '"' (!'"' ('\\"'  /  .))* '"'
+ID   <-   LETTER (LETTER  /  DIGIT)*
+LETTER   <-   [a-zA-Z\u0080-\u00FF_]
+HTML_STRING   <-   '<' (TAG  /  (![<>] .))* '>'
+TAG   <-   '<' .*? '>'
+COMMENT   <-   '/*' .*? '*/'
+LINE_COMMENT   <-   '//' .*? '\r'? '\n'
+PREPROC   <-   '#' (![\r\n] .)*
+WS   <-   [ \t\n\r]+
+		]]
+
+		assert(g)
+
+		local lpegParser = Coder.makeg(g)
+        s = [["s"    --  <<Fk!>>]] 
+
+        local r, lab, errpos = lpegParser:match(s)
+        print(r, lab, errpos)
+        assert.equal(lpegParser:match(s), #s + 1)
+
+    end)
 
 
 end)
